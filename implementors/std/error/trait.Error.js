c6a4b9f9a55d578fb8868dd0530eddf4c6301e96
SpiralP <SpiralP@users.noreply.github.com>
2020-10-10 01:13:08 +0000

    deploy: 6165273f8eb72f3553635e6f7cac4c3d882944ac

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -4,5 +4,6 @@ implementors["futures_executor"] = [{"text":"impl Error for EnterError","synthet
 implementors["futures_task"] = [{"text":"impl Error for SpawnError","synthetic":false,"types":[]}];
 implementors["futures_util"] = [{"text":"impl Error for Aborted","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;Any, Item&gt; Error for ReuniteError&lt;T, Item&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;Any&gt; Error for ReuniteError&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["log"] = [{"text":"impl Error for SetLoggerError","synthetic":false,"types":[]},{"text":"impl Error for ParseLevelError","synthetic":false,"types":[]}];
+implementors["proc_macro2"] = [{"text":"impl Error for LexError","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
