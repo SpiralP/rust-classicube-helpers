487e870500049be2f3c9ec8d55240385d932c95d
SpiralP <SpiralP@users.noreply.github.com>
2020-12-17 07:19:20 +0000

    deploy: 4764cc727d8ff2f34eb43d36733002a5a1582068

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {};
 implementors["classicube_sys"] = [{"text":"impl&lt;Storage:&nbsp;Ord, Align:&nbsp;Ord&gt; Ord for __BindgenBitfieldUnit&lt;Storage, Align&gt;","synthetic":false,"types":[]}];
 implementors["futures_util"] = [{"text":"impl&lt;T:&nbsp;Ord&gt; Ord for AllowStdIo&lt;T&gt;","synthetic":false,"types":[]}];
-implementors["log"] = [{"text":"impl Ord for Level","synthetic":false,"types":[]},{"text":"impl Ord for LevelFilter","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Ord for Metadata&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Ord for MetadataBuilder&lt;'a&gt;","synthetic":false,"types":[]}];
 implementors["proc_macro2"] = [{"text":"impl Ord for Ident","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl Ord for Lifetime","synthetic":false,"types":[]}];
+implementors["tracing_core"] = [{"text":"impl Ord for Level","synthetic":false,"types":[]},{"text":"impl Ord for LevelFilter","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["futures_util"] = [{"text":"impl&lt;T:&nbsp;AsRef&lt;[u8]&gt;&gt; AsRef&lt;[u8]&gt; for Window&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["tracing_core"] = [{"text":"impl AsRef&lt;str&gt; for Field","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
