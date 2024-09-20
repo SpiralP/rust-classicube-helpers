cd23fc4f97de0df256da79138f97326007a1ed66
SpiralP <spiral.palmer@gmail.com>
2024-09-20 14:46:13 -0700

    reorder method on event handlers, so you can run before/after other handlers

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[doc(hidden)]
 #[macro_export]
 macro_rules! make_event_handler {
@@ -61,13 +63,55 @@ macro_rules! make_event_handler {
                     }
                 }
 
+                #[must_use]
+                pub fn index(&self) -> Option<usize> {
+                    let event = unsafe { & ::classicube_sys::[<$event_type Events>].$event_name };
+                    let ptr: *const $crate::callback_handler::CallbackHandler<[<$event_name Event>]> =
+                        self.callback_handler.as_ref();
+                    let ptr: *const ::std::os::raw::c_void = ptr.cast();
+
+                    #[allow(clippy::cast_sign_loss)]
+                    for i in 0..event.Count as usize {
+                        let handler = event.Handlers[i];
+                        let obj: *const ::std::os::raw::c_void = event.Objs[i].cast();
+                        #[allow(clippy::fn_address_comparisons)]
+                        if handler
+                            .map(|handler| handler == Self::callback)
+                            .unwrap_or(false)
+                            && obj == ptr
+                        {
+                            return Some(i);
+                        }
+                    }
+
+                    None
+                }
+
+                /// Reorder by index
+                #[must_use]
+                pub fn reorder(&self, new_index: usize) -> Option<()> {
+                    let old_index = self.index()?;
+
+                    let event = unsafe { &mut ::classicube_sys::[<$event_type Events>].$event_name };
+                    #[allow(clippy::cast_sign_loss)]
+                    let count = event.Count as usize;
+
+                    let handlers = &mut event.Handlers[..count];
+                    let objs = &mut event.Objs[..count];
+                    $crate::macros::reorder(handlers, old_index, new_index);
+                    $crate::macros::reorder(objs, old_index, new_index);
+
+                    Some(())
+                }
+
                 unsafe fn register_listener(&mut self) {
                     if !self.registered {
+                        let handlers = &mut ::classicube_sys::[<$event_type Events>].$event_name;
                         let ptr: *mut $crate::callback_handler::CallbackHandler<[<$event_name Event>]> =
                             self.callback_handler.as_mut();
 
                         ::classicube_sys::[<Event_Register $func_type>](
-                            &mut ::classicube_sys::[<$event_type Events>].$event_name,
+                            handlers,
                             ptr.cast(),
                             Some(Self::callback),
                         );
@@ -124,6 +168,45 @@ macro_rules! make_event_handler {
             }
         }
     };
+}
+
+pub fn reorder<T>(slice: &mut [T], old_index: usize, new_index: usize) {
+    match new_index.cmp(&old_index) {
+        Ordering::Less => {
+            slice[new_index..=old_index].rotate_right(1);
+        }
+        Ordering::Greater => {
+            slice[old_index..=new_index].rotate_left(1);
+        }
+        Ordering::Equal => {}
+    }
+}
+
+#[test]
+fn test_reorder() {
+    //           0  1  2  3  4  5  6  7  8  9
+    let mut a = [2, 3, 4, 5, 6, 7, 8, 0, 0, 0];
+    let count = 7;
+
+    // move 7 to where 4 is, shift 4,5,6 to the right
+    reorder(&mut a[..count], 5, 2);
+    //               0  1  2  3  4  5  6  7  8  9
+    assert_eq!(&a, &[2, 3, 7, 4, 5, 6, 8, 0, 0, 0]);
+
+    // move 7 to where 6 is, move 4,5,6 to the left
+    reorder(&mut a[..count], 2, 5);
+    //               0  1  2  3  4  5  6  7  8  9
+    assert_eq!(&a, &[2, 3, 4, 5, 6, 7, 8, 0, 0, 0]);
+
+    // move 2 to where 3 is, and 3 to where 2 is
+    reorder(&mut a[..count], 0, 1);
+    //               0  1  2  3  4  5  6  7  8  9
+    assert_eq!(&a, &[3, 2, 4, 5, 6, 7, 8, 0, 0, 0]);
+
+    // do nothing
+    reorder(&mut a[..count], 0, 0);
+    //               0  1  2  3  4  5  6  7  8  9
+    assert_eq!(&a, &[3, 2, 4, 5, 6, 7, 8, 0, 0, 0]);
 }
 
 #[macro_export]
